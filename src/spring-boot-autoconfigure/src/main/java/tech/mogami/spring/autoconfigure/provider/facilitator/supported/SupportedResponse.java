6e904ecf17ab8f3f317e4d31fc4c3e45a95dce30
straumat <github.backlog426@passmail.net>
2025-05-20 09:48:13 +0200

    Simplify SupportedResponse object

--- src/spring-boot-autoconfigure/src/main/java/tech/mogami/spring/autoconfigure/provider/facilitator/supported/SupportedResponse.java
+++ src/spring-boot-autoconfigure/src/main/java/tech/mogami/spring/autoconfigure/provider/facilitator/supported/SupportedResponse.java
@@ -1,6 +1,7 @@
 package tech.mogami.spring.autoconfigure.provider.facilitator.supported;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
@@ -9,10 +10,25 @@ import java.util.List;
  *
  * @param kinds list of supported (scheme, network) pairs
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Jacksonized
+@Builder
 public record SupportedResponse(
         List<SupportedKind> kinds
 ) {
+
+    /**
+     * Single pair the facilitator can handle.
+     *
+     * @param x402Version x402 version
+     * @param scheme      the scheme used for the payment
+     * @param network     the network used for the payment
+     */
+    public record SupportedKind(
+            int x402Version,
+            String scheme,
+            String network) {
+    }
+
 }
 
 

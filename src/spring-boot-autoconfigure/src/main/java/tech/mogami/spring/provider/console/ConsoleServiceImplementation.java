e3649af42f84c5e0637914dcf4023ecf6c7d647e
straumat <stephane.traumat@mogami.tech>
2025-10-28 15:27:42 +0100

    Remove console logs

--- src/spring-boot-autoconfigure/src/main/java/tech/mogami/spring/provider/console/ConsoleServiceImplementation.java
+++ src/spring-boot-autoconfigure/src/main/java/tech/mogami/spring/provider/console/ConsoleServiceImplementation.java
@@ -7,9 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import tech.mogami.commons.api.console.v1.EventRequest;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static tech.mogami.commons.api.console.ConsoleApiEndpoints.MOGAMI_X402_CONSOLE_API_BASE_URL;
-import static tech.mogami.commons.api.console.ConsoleApiEndpoints.V1.EVENTS_ENDPOINT;
 
 /**
  * {@link ConsoleService} implementation.
@@ -34,19 +32,7 @@ public class ConsoleServiceImplementation implements ConsoleService {
 
     @Override
     public final void logEvent(final EventRequest event) {
-        client.post()
-                .uri(EVENTS_ENDPOINT)
-                .contentType(APPLICATION_JSON)
-                .bodyValue(event)
-                .retrieve()
-                .bodyToMono(String.class)
-                .doOnNext(eventId ->
-                        log.info("{}: Event logged successfully for nonce: {} with event id: {}", event.type(), event.nonce(), eventId)
-                )
-                .doOnError(e ->
-                        log.error("{}: Failed to log event: {}", event.type(), e.getMessage(), e)
-                )
-                .subscribe();
+        log.debug("Received event: {}", event);
     }
 
 }

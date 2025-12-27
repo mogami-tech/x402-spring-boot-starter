b5d226863e30f838b32bd3d06e5bcadbddd857a7
straumat <stephane.traumat@mogami.tech>
2025-12-27 14:11:57 +0100

    All tests are now working

--- src/spring-boot-autoconfigure/src/test/java/tech/mogami/spring/test/core/controllers/WeatherControllerTest.java
+++ src/spring-boot-autoconfigure/src/test/java/tech/mogami/spring/test/core/controllers/WeatherControllerTest.java
@@ -17,7 +17,6 @@ import tech.mogami.spring.test.util.BaseTest;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -216,51 +215,38 @@ public class WeatherControllerTest extends BaseTest {
     @Test
     @DisplayName("get /weather with valid payment header")
     void getWeatherWithValidPaymentHeader() throws Exception {
-        fail("TODO Fix this test");
-//        // Calling the API with the payment header.
-//        var result = mockMvc.perform(get("/weather").header(X402_PAYMENT_REQUIRED_HEADER, getSampleEncodedPaymentHeader("isValidTrue")))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("It's sunny!"))
-//                .andReturn();
+        // Getting the payment required to build a payment payload =====================================================
+        var result = mockMvc.perform(get("/weather"))
+                .andExpect(status().isPaymentRequired())
+                .andReturn();
+        var PaymentRequired = X402V2Client.extractPaymentRequired(getHeaders(result.getResponse()))
+                .orElseThrow(() -> new IllegalStateException("PaymentRequired should be present"));
 
-        // Testing the decoded payment payload received in the response.
-//        assertThat((PaymentPayload) result.getRequest().getAttribute(X402_X_PAYMENT_HEADER_DECODED))
-//                .isNotNull()
-//                .satisfies(paymentPayload -> {
-//                    assertThat(paymentPayload.x402Version()).isEqualTo(X402_SUPPORTED_VERSION_BY_MOGAMI.version());
-//                    assertThat(paymentPayload.scheme()).isEqualTo(EXACT_SCHEME.name());
-//                    assertThat(paymentPayload.network()).isEqualTo(BASE_SEPOLIA.name());
-//                    assertThat((ExactSchemePayload) paymentPayload.payload())
-//                            .isNotNull()
-//                            .satisfies(payload -> {
-//                                assertThat(payload).isInstanceOf(ExactSchemePayload.class);
-//                                assertThat(payload.signature()).isEqualTo("0x1c7e56451968cc2c2816fc776c6f75483815408b2e087d568ce7e8509c59911b3c9353dbdff8b565680e9defd52336eb2213dfd83f1a07c20625e53d8fda2b951b");
-//                                assertThat(payload.authorization().from()).isEqualTo("0x857b06519E91e3A54538791bDbb0E22373e36b66");
-//                                assertThat(payload.authorization().to()).isEqualTo("0x2980bc24bBFB34DE1BBC91479Cb712ffbCE02F73");
-//                                assertThat(payload.authorization().value()).isEqualTo("1000");
-//                                assertThat(payload.authorization().validAfter()).isEqualTo("1747486410");
-//                                assertThat(payload.authorization().validBefore()).isEqualTo("1747486530");
-//                                assertThat(payload.authorization().nonce()).isEqualTo("isValidTrue");
-//                            });
-//                });
+        // Building a valid payment payload ============================================================================
+        var paymentPayload = X402V2Client.buildPaymentPayload(
+                PaymentRequired,
+                PaymentRequired.accepts().getFirst(),
+                Credentials.create(TEST_CLIENT_WALLET_ADDRESS_1_PRIVATE_KEY)
+        );
+        final Map<String, String> paymentHeaders = X402V2Client.buildPaymentHeaders(paymentPayload);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(X402_PAYMENT_SIGNATURE_HEADER, paymentHeaders.get(X402_PAYMENT_SIGNATURE_HEADER));
 
-        // Testing that the response contains the X-PAYMENT-RESPONSE header.
-//        try {
-//            var decodeSettleString = new String(Base64.getMimeDecoder().decode(result.getResponse().getHeader(X402_PAYMENT_REQUIRED_HEADER)), UTF_8);
-//            var settleResponse = new ObjectMapper().readValue(decodeSettleString, SettleResponse.class);
-//            assertThat(settleResponse)
-//                    .isNotNull()
-//                    .satisfies(resultSettle -> {
-//                        assertThat(resultSettle.success()).isTrue();
-//                        assertThat(resultSettle.network()).isEqualTo(BASE_SEPOLIA.name());
-//                        assertThat(resultSettle.transaction()).isEqualTo("0x7cbf21c639f7bcd8e68ba02b83b34187f686577a0cead0d7c6f0f57183a84b51");
-//                        assertThat(resultSettle.errorReason()).isEqualTo("invalid_scheme");
-//                        assertThat(resultSettle.payer()).isEqualTo("0x2980bc24bBFB34DE1BBC91479Cb712ffbCE02F73");
-//                    });
-//        } catch (IOException e) {
-//            fail("Invalid X-PAYMENT-RESPONSE header", e);
-//        }
+        // Calling the API with the payment header.
+        result = mockMvc.perform(get("/weather").headers(headers))
+                .andExpect(status().isOk())
+                .andExpect(content().string("It's sunny!"))
+                .andReturn();
+
+        // Checking the response =======================================================================================
+        assertThat(X402V2Client.extractSettlementResponse(getHeaders(result.getResponse())))
+                .isPresent().get()
+                .satisfies(settlementResponse -> {
+                    assertThat(settlementResponse.success()).isTrue();
+                    assertThat(settlementResponse.errorReason()).isBlank();
+                    assertThat(settlementResponse.payer()).isEqualToIgnoringCase(TEST_CLIENT_WALLET_ADDRESS_1);
+                    assertThat(settlementResponse.network()).isEqualTo(BASE_SEPOLIA.networkId());
+                });
     }
 
 }
